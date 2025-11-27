05c7bbbf9a84483696b97342ce9f6a35f6aa6c26
Kaushiki <kaushikichoudhary3@gmail.com>
2025-11-27 12:34:15 +0000

    Arrays

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,3 +1,5 @@
+// Brute force
+
 public class TwoSum {
     public static void main(String[] args) {
 
@@ -14,3 +16,21 @@ public class TwoSum {
         }
     }
 }
+
+
+// OPTIMIZED SOLUTION USING HASHMAP
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+
+//         HashMap<Integer, Integer> map= new HashMap<>();
+//         for(int i=0; i<nums.length; i++){
+//             int compliment = target - nums[i];
+//             if(map.containsKey(compliment)){
+//                 return new int[]{map.get(compliment), i};
+//             }
+//             map.put(nums[i], i);
+//         }
+//         return new int[]{};
+//     }
+// }
